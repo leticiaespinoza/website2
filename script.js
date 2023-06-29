23ac8f63b8a84b818704fc9b30dc72ef405e26bd
leticiaespinoza <138073763+leticiaespinoza@users.noreply.github.com>
2023-06-29 09:53:37 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+var button = document.getElementById("hi");
+var h1 = document.getElementById("estefanyisha")
+var body = document.body;
+
+body.setAttribute("style","background-color: purple")
+
+
+
+button.addEventListener("click", function(){
+console.log("click");
+
+body.setAttribute("style","background-color: blue");
+
+});
+
+
+
+
+
